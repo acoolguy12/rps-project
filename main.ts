4ca23456607a6c090008b4c98da1a4470b6a3c2b
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-01-31 11:50:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 function updateScoreboard () {
     OLED.clear()
-    OLED.writeStringNewLine("Player A " + playerA)
+    OLED.writeStringNewLine("Player A: " + playerA)
     OLED.newLine()
-    OLED.writeStringNewLine("Player B" + playerB)
+    OLED.writeStringNewLine("Player B: " + playerB)
     OLED.newLine()
-    OLED.writeStringNewLine("ties" + ties)
+    OLED.writeStringNewLine("ties: " + ties)
     OLED.newLine()
-    OLED.writeStringNewLine("rounds " + rounds)
+    OLED.writeStringNewLine("rounds: " + rounds)
 }
 input.onButtonPressed(Button.A, function () {
     playerA += 1
     rounds += 1
+    updateScoreboard()
     basic.showLeds(`
         . # # # .
         . # . # .
@@ -22,6 +23,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     ties += 1
     rounds += 1
+    updateScoreboard()
     basic.showLeds(`
         . # # # .
         . . # . .
@@ -33,23 +35,30 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     playerB += 1
     rounds += 1
+    updateScoreboard()
     basic.showLeds(`
-        . # # # .
+        . # # . .
         . # . # .
         . # # # .
         . # . # .
-        . # # # .
+        . # # . .
         `)
 })
-let ties = 0
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    playerB = 0
+    playerA = 0
+    rounds = 0
+    ties = 0
+    OLED.writeStringNewLine("\"Lets play some RPS\"")
+    basic.pause(2000)
+    updateScoreboard()
+}
 let rounds = 0
-let playerA = 0
+let ties = 0
 let playerB = 0
-OLED.init(128, 64)
-playerB = 0
-playerA = 0
-rounds = 0
-ties = 0
-OLED.writeStringNewLine("\"Lets play some RPS\"")
-basic.pause(2000)
-updateScoreboard()
+let playerA = 0
+reset()
